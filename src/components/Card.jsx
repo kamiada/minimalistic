333f538916a6cebeb597513f1b468ae71011c7a9
kamiada <kamiada528@gmail.com>
2021-02-04 20:49:56 +0000

    attempt at making somethinbg minimalistic

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '/components.scss';
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div className="card_wrapper">
+
+            </div>
+        )
+    }
+}
