16a4cd28fe5e077629a59d9513fc933e289b66ad
kamiada <kamiada528@gmail.com>
2021-02-05 11:31:32 +0000

    initial set up done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import Card from './components/Card.jsx';
+import "./App.css";
+import Card from "./components/Card.jsx";
 
 function App() {
   return (
     <div className="App">
-      <div className="cardholder">
-      <Card title="skills."/>
+    <header>
+      <h1 >Hi, my name is Adrianna</h1>
+    </header>
+    <body>
+    <div className="cardholder">
+        <Card onClick={() => console.log("click")} title="skills." />
+        <Card onClick={() => console.log("click")} title="about." />
+        <Card onClick={() => console.log("click")} title="projects." />
       </div>
+    </body>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
-import './components.scss';
+import React from "react";
+import "./components.scss";
 
-export default class Card extends Component {
-    render() {
-        return (
-            <div className="card_wrapper">
-                <div className="card_wrapper--card">
-                {this.props.title}
-                </div>       
-            </div>
-        )
-    }
-}
+const Card = ({ title, onClick, href }) => (
+  <div className="card_wrapper">
+    <div className="card_wrapper--card">
+      <button className="title_placement" onClick={onClick}>{title}</button>
+    </div>
+  </div>
+);
+export default Card;
