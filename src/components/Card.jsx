3eba9861e320bc0a319a920f24a64c9cd51b8ea4
kamiada <kamiada528@gmail.com>
2021-02-07 18:36:16 +0000

    clean up

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./components.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 
 const Card = ({ title, onClick, href }) => (
