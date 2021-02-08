08f1319f26e89413c32b282845140c8e92cbe491
kamiada <kamiada528@gmail.com>
2021-02-08 15:45:24 +0000

    set up for skills

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Segment } from './Segment';
 export { default as Card } from './Card';
 export { default as NavBar } from './NavBar';
+export { default as Toggle } from './Toggle';
+export { default as Tag } from './Tag';
