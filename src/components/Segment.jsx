e42d71db97352e3e601f8e9e6dbcaf1593feee9c
kamiada <kamiada528@gmail.com>
2021-02-20 13:18:38 +0000

    na na na na batman

--- src/components/Segment.jsx
+++ src/components/Segment.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import "./components.scss";
 import { BrowserRouter as Link } from "react-router-dom";
 import GifPlayer from 'react-gif-player';
-
+import Tag from './Tag';
 
 class Segment extends Component {
   render() {
     return (
       <div className="segment">
         <GifPlayer
-          gif={'../gifs_examples/periodicTableUpdate.gif'}
+          gif={this.props.gif}
           pauseRef={(pause) => (this.pauseGif = pause)}
         />
         <Link to={this.props.href}>
+          <p>
           {this.props.title} {this.props.description}
+          </p>
         </Link>
       </div>
     );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import "./pages.scss";
 import { Segment, Tag, Navbar} from '../components';
+import PeriodicTable from '../gifs_examples/periodicTableUpdate.gif';
 
 class Projects extends Component {
   render() {
     return (
       <body>
         <div className="projects_container">
-          <Segment />
+          <Segment 
+            gif={PeriodicTable}
+            title="Periodic Table"
+            description="Project done in React.js"
+          />
         </div>
       </body>
     );
