530d5643a2938901ecc9c936ef4589c2a67660b1
kamiada <kamiada528@gmail.com>
2021-02-22 19:21:24 +0000

    more changes

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import "./pages.scss";
 
 class Minipage extends Component {
-render() {
+  render() {
     return (
+      <body>
         <div className="page_container">
-            Testing
-            <p>{this.props.text}</p>
+          Testing
+          <p>{this.props.text}</p>
         </div>
-    )
-}
+      </body>
+    );
+  }
 }
 export default Minipage;
