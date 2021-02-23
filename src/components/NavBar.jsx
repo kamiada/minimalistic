362c0907701f35139b72d49beb1baa77fd9a1dce
kamiada <kamiada528@gmail.com>
2021-02-23 00:14:11 +0000

    just descriptions to be added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./components.scss";
 import { Home, Skills, Projects, About, ProjectMiniPage } from "../pages";
-import GithubIcon from '../icons/GitHub-Mark-Light-64px.png';
-import Linkedin from '../icons/whiteLinkedin.png';
+import GithubIcon from "../icons/GitHub-Mark-Light-64px.png";
+import Linkedin from "../icons/whiteLinkedin.png";
 import data from "../Posts/posts.json";
 class Navbar extends Component {
   render() {
@@ -28,43 +28,50 @@ class Navbar extends Component {
                 <a href="/about">About</a>
               </li>
               <div className="social_parent">
-              <li>
-              <a className="socialIcon--linkedin" href="https://www.linkedin.com/in/adrianna-kaminska-19985a173/">
-                <img src={Linkedin} alt="linkedin link" />
-              </a>
-              </li>
-              <li>
-              <a className="socialIcon" href="https://github.com/kamiada">
-                <img src={GithubIcon} alt="github link" />
-              </a>
-              </li>
+                <li>
+                  <a
+                    className="socialIcon--linkedin"
+                    href="https://www.linkedin.com/in/adrianna-kaminska-19985a173/"
+                  >
+                    <img src={Linkedin} alt="linkedin link" />
+                  </a>
+                </li>
+                <li>
+                  <a className="socialIcon" href="https://github.com/kamiada">
+                    <img src={GithubIcon} alt="github link" />
+                  </a>
+                </li>
               </div>
             </ul>
           </nav>
           <Switch>
-            <Route exact path="/" component={Home}>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/skills" component={Skills}></Route>
+            <Route exact path="/projects" component={Projects}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route
+              path="/projects/periodicTable"
+              component={ProjectMiniPage}
+            ></Route>
+            <Route path="/projects/Live" component={ProjectMiniPage}></Route>
+            <Route
+              path="/projects/CustomisedTourism"
+              component={ProjectMiniPage}
+            ></Route>
+            <Route path="/projects/Tracker" component={ProjectMiniPage}></Route>
+            <Route
+              path="/projects/PandaMediator"
+              component={ProjectMiniPage}
+            ></Route>
+            <Route path="/projects/Arctica">
+              <ProjectMiniPage
+                id={data.projectsDescriptions[5]._id}
+              />
             </Route>
-            <Route path="/skills" component={Skills}>
-            </Route>
-            <Route exact path="/projects" component={Projects}>
-            </Route>
-            <Route path="/about" component={About}>
-            </Route>
-            <Route path="/projects/periodicTable" component={ProjectMiniPage}>
-            </Route>
-            <Route path="/projects/Live" component={ProjectMiniPage}>
-            </Route>
-            <Route path="/projects/CustomisedTourism" component={ProjectMiniPage}>
-            </Route>
-            <Route path="/projects/Tracker" component={ProjectMiniPage}>
-            </Route>
-            <Route path="/projects/PandaMediator" component={ProjectMiniPage}>
-            </Route>
-            <Route path="/projects/Arctica" >
-            <ProjectMiniPage text={data.projectsDescriptions[0]._id} />
-            </Route>
-            <Route path="/projects/FeelMyFeels" component={ProjectMiniPage}>
-            </Route>
+            <Route
+              path="/projects/FeelMyFeels"
+              component={ProjectMiniPage}
+            ></Route>
           </Switch>
         </Fragment>
       </Router>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import "./pages.scss";
 import data from "../Posts/posts.json";
 
-//props which will take in id, id will match with the id on the json file - this file is going to be loaded into this Project Page
-
 class Minipage extends Component {
   render() {
+    console.log(Number.parseInt(this.props.id));
     return (
       <body>
         <div className="page_container">
-         {this.props.id ? <p>{data.projectsDescriptions[this.props.id.parseInt()].post}</p> : 'There is no post assosciated wit this project'}
-          <p>{this.props.text}</p>
+          {this.props.id && this.props.id ? (
+            <p>{data.projectsDescriptions[Number.parseInt(this.props.id)].post}</p>
+          ) : (
+            "There is no post assosciated wit this project"
+          )}
         </div>
       </body>
     );
