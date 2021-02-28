a0d2434e1374f5dfff1769ddd02770d50f6cc37a
kamiada <kamiada528@gmail.com>
2021-02-28 10:52:58 +0000

    footer and making titles in projects looks nicer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./components.scss";
+
+class Footer extends Component {
+    render(){
+        return(
+            <div className="footer">
+                 <span>&#169;</span> Adrianna Kaminska
+            </div>
+        )
+    }
+}
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {default as AnimatedTitle} from './AnimatedTitle.jsx';
 export {default as Toggle} from './Toggle.jsx';
 export {default as Panel} from './Panel.jsx';
 export {default as WidePanel} from './WidePanel.jsx';
+export {default as Footer} from './Footer.jsx';
