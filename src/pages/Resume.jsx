3ed0b7398ad57cb9c9958a6e0dd747383c767195
kamiada <kamiada528@gmail.com>
2021-03-14 14:15:34 +0000

    still not getting what is the issue with the pdf

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./pages.scss";
-import { Document } from "react-pdf";
 import pdf from '../docs/CV3.pdf';
+import { Document,pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+const path = '../docs/CV3.pdf';
 class Resume extends Component {
   render() {
     return (
       <div className="resume_container">
-        <Document file={pdf} />
+        <Document file={path} />
       </div>
     );
   }
